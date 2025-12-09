90d259185cadcd10cffc3a7d520e13fdfeaf3103
adamsir <donuteeze@gmail.com>
2025-12-10 00:35:47 +0100

    feat(planets): add basic page with planets listing

--- src/app/planets/page.tsx
+++ src/app/planets/page.tsx
@@ -1,12 +1,23 @@
-import Image from "next/image";
 import styles from "@/app/page.module.css";
-import Link from "next/link";
+import { getPlanets } from "@/lib/data";
 
-export default function Planets() {
+export default async function Planets() {
+  const planets = await getPlanets();
+
+  if (!planets) {
+    return <div>No planets found</div>;
+  }
+
   return (
     <div className={styles.page}>
       <main className={styles.main}>
         <h1>Welcome to Mars</h1>
+        {planets.map((planet, index) => (
+          <div key={index}>
+            <h2>{planet.name}</h2>
+            <p>{planet.terrain}</p>
+          </div>
+        ))}
       </main>
     </div>
   );
---
+++ src/lib/data.ts
@@ -0,0 +1,7 @@
+import { type Planet } from "@/types/planets";
+
+export async function getPlanets() {
+  const response = await fetch("https://swapi.info/api/planets");
+  const data: Planet[] = await response.json();
+  return data;
+}
